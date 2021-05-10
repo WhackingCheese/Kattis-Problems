a6ac56d0ef2d56a327f711b4198fcb49adf3f5c1
Mikolaj Cymcyk <mic5@hi.is>
2021-05-10 09:37:11 +0000

    Readme Update

--- generate_readme.py
+++ generate_readme.py
@@ -1,22 +1,23 @@
 import json
 
-REPO = "https://github.com/WhackingCheese/Kattis-Problems/blob/main/problems/"
 problems = json.loads(open("problems.json", "r", encoding="utf-8").read())
 README = open("README.md", "w", encoding="utf-8")
 
-text = "Problem | Solution | Languange\n--- | --- | --- \n"
+text = "# My solutions to kattis competitive programming problems\n\nProblem | Solution | Languange\n:--- | :--- | :--- \n"
 for problem in problems["problems"]:
     if "url" in problem:
         text += f"[{problem['name']}]({problem['url']}{problem['id']})"
     else:
         text += f"[{problem['name']}]({problems['kattisURL']}{problem['id']})"
     text += " | "
-    text += f"[{problem['id']}]({REPO}/{problem['id']}.{problem['lang']})"
+    text += f"[{problem['id']}](problems/{problem['id']}.{problem['lang']})"
     text += " | "
     if problem['lang'] == 'py':
         text += "Python 3"
-    else:
+    elif problem['lang'] == 'c':
         text += "C"
+    else:
+        text += problem['lang'].capitalize()
     text += "\n"
 
 README.write(text)
